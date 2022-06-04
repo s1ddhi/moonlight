610aaab54d214b53fc05a9a13e8dd56988bcc9ed
Siddhi Aphiphunya <siddhi@hey.com>
2022-06-04 14:59:10 +0100

    ♻️ [SA] utility function refactor

--- src/endpoints/lending.js
+++ src/endpoints/lending.js
@@ -9,6 +9,8 @@ const { loadDB, findByUserID, findTotals, findUserProportions, findUserIDBalance
 
 const axios = require('axios');
 
+const { userBalanceAggregator, proportionAndUpdateWithdraw, proportionAndUpdateLPDeposit, updateBaseDeposit, buildBaseDeposit, buildAccruedInterest, userBalanceDocument } = require('../utilities');
+
 app.get('/', (_, res) => {
     res.send('Hello World!');
 });
@@ -146,112 +148,6 @@ app.get('/updateWithTodayActivity', async (_, res) => {
     res.send("Completed batched deposit and withdraw as well as updating new balances of the day")
 });
 
-const userBalanceAggregator = async (user, currency) => {
-    const loadedDb = await loadDB();
-    const balance = (await findByUserID(loadedDb, 'userBalances', user))[0];
-    const todaysActivity = await findToday(loadedDb, 'ledger');
-
-    const deposits = todaysActivity.filter(entry => entry.type == 'deposit' && entry.user == user);
-    const totalDeposits = deposits.reduce(function (acc, entry) {
-        acc.dai += entry.amount.dai;
-        acc.usdc += entry.amount.usdc;
-        acc.usdt += entry.amount.usdt;
-        return acc;
-    }, { dai: 0, usdc: 0, usdt: 0});
-
-    const withdraws = todaysActivity.filter(entry => entry.type == 'withdraw' && entry.user == user);
-    const totalLPWithdrawn = withdraws.reduce(function (acc, entry) {
-        return acc + entry.lpAmount;
-    }, 0)
-
-    prices = await getSpotPrice(currency);
-
-    const currentBalance = (prices['lp-3pool-curve'][currency] * balance.baseDeposit.lp);
-    const accruedBalance = (prices['lp-3pool-curve'][currency] * balance.accruedInterest.baseLP) + (prices['curve-dao-token'][currency] * balance.accruedInterest.crv);
-
-    const finalDepositBalance = (prices['dai'][currency] * totalDeposits.dai) + (prices['usd-coin'][currency] * totalDeposits.usdc) + (prices['tether'][currency] * totalDeposits.usdt);
-    const finalWithdrawnBalance = prices['lp-3pool-curve'][currency] * totalLPWithdrawn;
-
-    const baseDepositBalance = currentBalance + finalDepositBalance - finalWithdrawnBalance;
-
-    return {
-        baseDepositBalance,
-        accruedBalance,
-        currency
-    };
-}
-
-const coingeckoAPI = 'https://api.coingecko.com/api/v3/simple/price?ids=dai%2Ctether%2Cusd-coin%2Clp-3pool-curve%2Cconvex-finance%2Ccurve-dao-token&vs_currencies=';
-
-const getSpotPrice = async (currency) => {
-    return await axios
-        .get(coingeckoAPI + currency)
-        .then(res => {;
-            return res.data;
-        })
-        .catch(error => {
-            console.log('Issue with fetching Coingecko API', error);
-        });
-};
-
-const proportionAndUpdateWithdraw = async (_db, userWithdrawals, totalLPWithdrawals, withdrawalResult) => {
-    for (userKey in userWithdrawals) {
-        const proportion = userWithdrawals[userKey].lp / totalLPWithdrawals;
-        const balance = (await findByUserID(_db, 'userBalances', userKey))[0];
-
-        const daiReceived = withdrawalResult.realisedAssetBal.dai * proportion;
-        const usdcReceived = withdrawalResult.realisedAssetBal.usdc * proportion;
-        const usdtReceived = withdrawalResult.realisedAssetBal.usdt * proportion;
-        balance.baseDeposit = updateBaseDeposit(balance.baseDeposit, -userWithdrawals[userKey].lp, -daiReceived, -usdcReceived, -usdtReceived);
-
-        await updateDocument(_db, 'userBalances', balance);
-    };
-};
-
-const proportionAndUpdateLPDeposit = async (_db, userDeposits, totalDaiDeposits, depositResult) => {
-    for (const userKey in userDeposits) {
-        const proportion = userDeposits[userKey].dai / totalDaiDeposits; // Assumed that amount deposit for each stablecoins are equivalent
-        const balance = (await findByUserID(_db, 'userBalances', userKey))[0];
-
-        const lpReceived = depositResult.convexLPReceived * proportion;
-        balance.baseDeposit = updateBaseDeposit(balance.baseDeposit, lpReceived, 0, 0, 0);
-
-        await updateDocument(_db, 'userBalances', balance);
-    };
-};
-
-const updateBaseDeposit = (current, lp, dai, usdc, usdt) => {
-    current.lp += lp;
-    current.dai += dai;
-    current.usdc += usdc;
-    current.usdt += usdt;
-    return current;
-};
-
-const buildBaseDeposit = (lp, dai, usdc, usdt) => {
-    return {
-        lp,
-        dai,
-        usdc,
-        usdt
-    }
-};
-
-const buildAccruedInterest = (lp, crv) => {
-    return {
-        lp,
-        crv
-    }
-};
-
-const userBalanceDocument = (user, baseDeposit, accruedInterest) => {
-    return {
-        user,
-        baseDeposit,
-        accruedInterest
-    }
-};
-
 const TAKEHOME_PERCENT = 0.02;
 
 app.get('/userBalance', jsonParser, async (req, res) => {
@@ -261,7 +157,6 @@ app.get('/userBalance', jsonParser, async (req, res) => {
     }
 
     const userBalance = await userBalanceAggregator(req.body.user, req.body.currency);
-    console.log(userBalance);
     res.send(augmentUserBalance(userBalance, TAKEHOME_PERCENT));
 });
 
